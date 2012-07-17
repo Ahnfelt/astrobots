6f56eb814a6cfd3865905bfa67e5bca49a94088e
Michael Werk <mfwerk@gmail.com>
2012-07-18 01:40:11 +0200

    Unification

--- src/com/astrobotsgame/typesystem/TypeErrorException.java
+++
@@ -1,9 +0,0 @@
-package com.astrobotsgame.typesystem;
-
-import com.astrobotsgame.syntax.Term;
-import com.astrobotsgame.syntax.TypeError;
-
-public class TypeErrorException extends RuntimeException {
-    public TypeErrorException(Term term, TypeError typeError) {
-    }
-}
